152e4e78dd99bc9a2de0cb560c752f948bc1de55
zjx <2426259881@qq.com>
2019-04-17 22:01:21 +0800

    创建视图，URLconf 创建模板

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include,url
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'index/', include('login.urls'))
+    url(r'', include('login.urls', namespace='login'))
 ]
--- django_project/login/views.py
+++ django_project/login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import BookInfo
+from django.template import loader
 
 # Create your views here.
 """
@@ -10,17 +11,30 @@ from .models import BookInfo
 
 
 def index(request):
-    return HttpResponse('首页')
+
+    # index1= loader.get_template('tempdemo/index.html')
+    #
+    # cont = {'username':'zjx'}
+    # # 使用变量参数渲染模板
+    # result = index1.render(cont)
+    # # 返回模板
+    # return HttpResponse(result)
+    return render(request, 'tempdemo/index.html', {'username': 'zjx'})
+
 
 
 def list(request):
-    return HttpResponse('列表页')
+    # return HttpResponse('列表页')
+    book = BookInfo.objects.all()
+    return render(request, 'tempdemo/list.html', {'book':book})
 
 
 def detail(request,id):
-    print(id)
-    try:
-        book = BookInfo.objects.get(pk=int(id))
-        return HttpResponse(book)
-    except:
-        return HttpResponse('请输入正确id')
+    # print(id)
+#     # try:
+#     #     book = BookInfo.objects.get(pk=int(id))
+#     #     return HttpResponse(book)
+#     # except:
+#     #     return HttpResponse('请输入正确id')
+    book = BookInfo.objects.get(pk=id)
+    return render(request, 'tempdemo/detail.html', {"book":book})
