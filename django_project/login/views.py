3aa5953752936f84b4aebe499d6dd6b94e5bad0a
zjx <2426259881@qq.com>
2019-04-17 15:15:27 +0800

    创建视图，URLconf

--- django_project/login/views.py
+++ django_project/login/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import BookInfo
 
 # Create your views here.
 """
 视图函数
 将函数和路由绑定
 """
+
+
+def index(request):
+    return HttpResponse('首页')
+
+
+def list(request):
+    return HttpResponse('列表页')
+
+
+def detail(request,id):
+    print(id)
+    try:
+        book = BookInfo.objects.get(pk=int(id))
+        return HttpResponse(book)
+    except:
+        return HttpResponse('请输入正确id')
